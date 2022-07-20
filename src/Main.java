5d32fd95e18269a795981d2ba33885712ce3e82a
DanilSinitsin <sinitsindanil90@mail.ru>
2022-07-20 15:12:09 +0500

    first commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        int start_Balance = 100;
+        int bonus;
+        int add = 100;
+        System.out.println(start_Balance + add);
+
+        int add1 = 1100;
+        bonus = 11;
+        if (add1 >= 100) {
+        } else {
+            bonus = 0;
+        }
+        int balance = start_Balance + add1 + bonus;
+        System.out.println(balance);
+
+
+    }
+}
+
